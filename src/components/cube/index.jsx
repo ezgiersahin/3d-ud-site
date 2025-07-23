5340d86c72091c9650e98a341b080fd4224027f1
Ezgi Erşahin <80217143+ezgiersahin@users.noreply.github.com>
2025-07-23 11:31:39 +0300

    sticky delete

--- src/components/cube/index.jsx
+++ src/components/cube/index.jsx
@@ -1,40 +1,31 @@
 "use client";
 
-import React, { useRef } from 'react';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { useSpring, useScroll, useTransform } from 'framer-motion';
 import { motion } from 'framer-motion-3d';
 import { useGLTF } from '@react-three/drei';
 import styles from './style.module.scss';
 
 export default function index() {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ['start start', 'end end']
-    });
-    const progress = useTransform(scrollYProgress, [0, 1], [0, 5]);
-    const smoothProgress = useSpring(progress, { damping: 20 });
-
     return (
-        <div ref={container} className={styles.main}>
+        <div className={styles.main}>
             <div className={styles.cube}>
                 <Canvas>
                     <OrbitControls enableZoom={false} enablePan={false} />
                     <ambientLight intensity={2} />
                     <directionalLight position={[2, 1, 1]} />
-                    <CustomModel progress={smoothProgress} />
+                    <CustomModel />
                 </Canvas>
             </div>
         </div>
     );
 }
 
-function CustomModel({ progress }) {
+function CustomModel() {
     const model = useGLTF('/assets/ud.glb');
     return (
-        <motion.mesh rotation-y={progress} rotation-x={progress}>
+        <motion.mesh>
             <primitive object={model.scene} scale={2.5} />
         </motion.mesh>
     );
